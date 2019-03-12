f8f02a52f419d8588b1739e22312a27d8d8e2814
benier <benier2u@etu.univ-lorraine.fr>
2019-03-12 09:36:52 +0100

    localisation des images

--- camera/app/components/Home.vue
+++ camera/app/components/Home.vue
@@ -1,81 +1,118 @@
 <template>
-    <Page>
-        <ActionBar title="Camera Tests FTW!"/>
-        <StackLayout>
-			<Button text="Take Picture" @tap="takePicture" />
-			<Image :src="img" width="75" height="75" />
-        </StackLayout>
-    </Page>
+  <Page>
+    <ActionBar title="Camera Tests FTW!"/>
+    <StackLayout>
+      <Button text="Take Picture" @tap="takePicture"/>
+      <Image :src="img" width="75" height="75"/>
+    </StackLayout>
+  </Page>
 </template>
 
 <template>
-    <Page>
-        <ActionBar title="Camera Tests FTW!"/>
-        <StackLayout>
-			<Button text="Take Picture" @tap="takePicture" />
-			<Button text="Choose Picture" @tap="selectPicture" />
-			<WrapLayout>
-				<Image v-for="img in images" :src="img.src" width="75" height="75" />
-			</WrapLayout>
-        </StackLayout>
-    </Page>
+  <Page>
+    <ActionBar title="Camera Tests FTW!"/>
+    <StackLayout>
+      <Button text="Take Picture" @tap="takePicture"/>
+      <Button text="Choose Picture" @tap="selectPicture"/>
+      <Button text="Get Current Location" col="1" textWrap="true" @tap="getLocation"/>
+      <!-- <text-field v-model="position.longitude"></text-field> -->
+      <text-field v-model="latitude"></text-field>
+      <WrapLayout>
+        <Image v-for="img in images" :src="img.src" width="75" height="75"/>
+      </WrapLayout>
+    </StackLayout>
+  </Page>
 </template>
 
 <script>
 import * as camera from "nativescript-camera";
 import * as imagepicker from "nativescript-imagepicker";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
 
 import { Image } from "tns-core-modules/ui/image";
 
 export default {
-	data() {
-		return {
-			images:[]
-		}
-	},
-	methods:{
-		selectPicture() {
+  data() {
+    return {
+      images: [],
+      watchIds: [],
+      locations: [],
+      latitude: "test"
+    };
+  },
+  methods: {
+    selectPicture() {
+      let context = imagepicker.create({
+        mode: "multiple"
+      });
 
-			let context = imagepicker.create({
-				mode: 'multiple' 
-			});
+      context
+        .authorize()
+        .then(function() {
+          return context.present();
+        })
+        .then(selection => {
+          selection.forEach(selected => {
+            console.log(JSON.stringify(selected));
 
-			context.authorize()
-			.then(function() {
-				return context.present();
-			})
-			.then(selection => {
-				selection.forEach(selected => {
-					
-					console.log(JSON.stringify(selected));
-
-					let img = new Image();
-					img.src = selected;
-					this.images.push(img);
-				});
-			}).catch(function (e) {
-				console.log('error in selectPicture', e);
-			});
-
-		},
-		takePicture() {
-			camera.requestPermissions()
-			.then(() => {
-				camera.takePicture({ width: 300, height: 300, keepAspectRatio: true, saveToGallery:false })
-				.then(imageAsset => {
-					let img = new Image();
-					img.src = imageAsset;
-					this.images.push(img);
-					console.log('ive got '+this.images.length+' images now.');
-				})
-				.catch(e => {
-					console.log('error:', e);
-				});
-			})
-			.catch(e => {
-				console.log('Error requesting permission');
-			});
-		}
-	}
-}
+            let img = new Image();
+            img.src = selected;
+            this.images.push(img);
+          });
+        })
+        .catch(function(e) {
+          console.log("error in selectPicture", e);
+        });
+    },
+    getLocation() {
+      let that = this;
+      geolocation
+        .getCurrentLocation({
+          desiredAccuracy: Accuracy.high,
+          maximumAge: 5000,
+          timeout: 10000
+        })
+        .then(
+          function(loc) {
+            if (loc) {
+							that.locations.push(loc);
+							alert(loc.longitude);
+							this.latitude=loc.latitude;
+							
+            }
+          },
+          function(e) {
+            console.log("Error: " + (e.message || e));
+          }
+        );
+    },
+    takePicture() {
+      this.getLocation();
+      camera
+        .requestPermissions()
+        .then(() => {
+          camera
+            .takePicture({
+              width: 300,
+              height: 300,
+              keepAspectRatio: true,
+              saveToGallery: false
+            })
+            .then(imageAsset => {
+              let img = new Image();
+              img.src = imageAsset;
+              this.images.push(img);
+              console.log("ive got " + this.images.length + " images now.");
+            })
+            .catch(e => {
+              console.log("error:", e);
+            });
+        })
+        .catch(e => {
+          console.log("Error requesting permission");
+        });
+    }
+  }
+};
 </script>
